041ed8597a9701b2eb78d8d5f9c7c27149146918
Dani <eltropersito@gmail.com>
2025-03-16 21:33:54 +0100

    Implementacion test testEquals

--- PersonaTest.java
+++ PersonaTest.java
@@ -19,5 +19,17 @@ public class PersonaTest {
         assertTrue(persona2.compareTo(persona1) > 0, "persona2 debería ser mayor que persona1");
         assertEquals(0, persona1.compareTo(new Persona(12345678)), "Dos personas con el mismo NIF deberían ser iguales");
     }
+
+    @Test
+    void testEquals() {
+        // Arrange
+        Persona persona1 = new Persona(12345678, "Ana García", 'F', 10, 5, 1995);
+        Persona persona2 = new Persona(12345678, "Ana García", 'F', 10, 5, 1995);
+        Persona persona3 = new Persona(87654321, "Luis Martínez", 'M', 20, 10, 1985);
+
+        // Act & Assert
+        assertEquals(persona1, persona2, "Dos personas con el mismo NIF deberían ser iguales");
+        assertNotEquals(persona1, persona3, "Dos personas con distinto NIF no deberían ser iguales");
+    }
 }
 
